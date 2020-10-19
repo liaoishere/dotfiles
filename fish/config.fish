f89dfa56234fb965ca89dbf3bbeb1128828b6e3d
Penghui Liao <liaoishere@gmail.com>
2020-10-19 18:22:29 +0800

    Update pipenv
    Signed-off-by: Penghui Liao <liaoishere@gmail.com>

--- fish/config.fish
+++ fish/config.fish
@@ -17,10 +17,8 @@ source $OMF_PATH/init.fish
 
 # pipenv
 set pipenv_fish_fancy yes
-
-# Cowsay
-# python -c "import sys, random; stdout = sys.stdout; sys.stdout = type('BlackHole', (), {'write': (lambda self, string: '')})(); import this; sys.stdout = stdout; print random.choice(''.join([this.d.get(i, i) for i in this.s]).splitlines()[2:]);" | cowsay -f www | lolcat
-# fortune -n 160 50% computers 50% linuxcookie | cowsay -s -W 50 -f www | lolcat
+eval (pipenv --completion)
+set -gx PIPENV_PYPI_MIRROR https://mirrors.ustc.edu.cn/pypi/web/simple
 
 # iterm2 shell integration
 # test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
